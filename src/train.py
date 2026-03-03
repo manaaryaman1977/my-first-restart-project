bddea871cd48191955612a322ac00fef95be0cb6
manaaryaman1977 <manaryaman1977@gmail.com>
2026-03-03 16:55:18 +0530

    Learnt to save our models

--- src/train.py
+++ src/train.py
@@ -1,6 +1,12 @@
 from sklearn.linear_model import LinearRegression
+import joblib
+import os
 
 def train_model(X, y):
     model = LinearRegression()
     model.fit(X, y)
+
+    os.makedirs("models", exist_ok=True)
+    joblib.dump(model, "models/linear_regression.pkl")
+
     return model
